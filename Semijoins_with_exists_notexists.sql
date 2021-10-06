c82f8df3139823a8597358d0af8b7e4f8b80f904
kavyaraviprakash <70050485+kavyaraviprakash@users.noreply.github.com>
2021-10-06 02:28:18 -0500

    Update Semijoins_with_exists_notexists.sql

--- Semijoins_with_exists_notexists.sql
+++ Semijoins_with_exists_notexists.sql
@@ -22,3 +22,63 @@ WHERE EXISTS (
          WHERE airc_model = 'Airbus A340'
                              AND pilot.pilot_ID = certified.pilot_ID
 );
+Q4.
+SELECT pilot_name
+FROM pilot
+WHERE EXISTS (
+       SELECT cert_date, pilot_id
+       FROM certified
+       WHERE airc_model = 'Airbus A340'
+                           AND pilot.pilot_ID = certified.pilot_ID
+);
+
+SELECT pilot_name
+FROM pilot
+WHERE EXISTS (
+       SELECT 'woah'
+       FROM certified
+       WHERE airc_model = 'Airbus A340'
+                           AND pilot.pilot_ID = certified.pilot_ID
+);
+
+ 
+
+SELECT pilot_name
+FROM pilot
+WHERE EXISTS (
+       SELECT 1
+       FROM certified
+       WHERE airc_model = 'Airbus A340'
+                           AND pilot.pilot_ID = certified.pilot_ID
+);
+
+Q5-------------------LEFT JOIN-------------------------------------
+SELECT pilot_name
+FROM pilot
+           JOIN certified ON pilot.pilot_ID = certified.pilot_ID
+WHERE certified.airc_model <> 'Airbus A340';
+
+SELECT pilot.pilot_id, certified.pilot_id, pilot_name, certified.airc_model
+FROM pilot JOIN certified ON pilot.pilot_ID = certified.pilot_ID;
+
+SELECT *
+FROM pilot
+           LEFT JOIN certified ON pilot.pilot_ID = certified.pilot_ID
+WHERE airc_model = 'Airbus A340';
+
+
+Q6.
+SELECT pilot_name
+FROM pilot
+WHERE pilot_id NOT IN (
+       SELECT certified.pilot_id
+       FROM certified
+       WHERE airc_model = 'Airbus A340');
+       
+ Q7.
+ SELECT airc_model
+FROM aircraft
+WHERE airc_model NOT IN (
+         SELECT flight.airc_model
+         FROM flight);
+         
